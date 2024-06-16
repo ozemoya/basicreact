409cfe0c0911835476a45b84fe47d5b1a4340260
kad <footnofetish123@gmail.com>
2024-06-15 22:26:45 -0400

    chore: Update image source in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
